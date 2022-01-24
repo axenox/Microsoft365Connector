775aedebfc5690b1473e15c4acdc303137d029df
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2022-01-24 12:05:55 +0100

    DOCS for MicrosoftOAuth2Authenticator added

--- CommonLogic/Security/Authenticators/MicrosoftOAuth2Autenticator.php
+++ CommonLogic/Security/Authenticators/MicrosoftOAuth2Autenticator.php
@@ -2,11 +2,46 @@
 namespace axenox\Microsoft365Connector\CommonLogic\Security\Authenticators;
 
 use axenox\OAuth2Connector\CommonLogic\Security\Authenticators\OAuth2Authenticator;
-use axenox\OAuth2Connector\CommonLogic\Security\AuthenticationToken\OAuth2RequestToken;
 use TheNetworg\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Token\AccessTokenInterface;
 use exface\Core\DataTypes\EncryptedDataType;
 
+/**
+ * Authenticates users using Azure Active Directory via OAuth 2.0.
+ * 
+ * To enable Single-Sign-On with Microsoft Azure, you need to register every
+ * required workbench installation (every URL) as an "Application" in the Azure 
+ * portal: see https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-v2-protocols.
+ * 
+ * You will need to provide a redirect URL: `https://yourdomain.com/path_to_workbench/api/oauth2client`.
+ * Make sure you use a secure HTTPS URL with a valid SSL certificate!
+ * 
+ * You will also need to select required scopes: the default scopes are `openid`, `profile`, `email`
+ * unless the `scopes` property of the authenticator is configured explicitly.
+ * 
+ * After the app registration is complete, you will get the `client_id` and the `client_secret`
+ * needed to configure the authenticator.
+ * 
+ * ## Example Configuration
+ * 
+ * ```
+ *  {
+ *      "class": "\\axenox\\Microsoft365Connector\\CommonLogic\\Security\\Authenticators\\MicrosoftOAuth2Autenticator",
+ *      "id": "AZURE_AD",
+ *      "name": "Azure AD",
+ *      "client_id": "552b11b2-586d-4154-a67b-c57af5a7ccce",
+ *      "client_secret": "fx2NG7jjy0JK8_8l.G-0lXup_T9F7W_iWm",
+ *      "create_new_users": true,
+ *      "create_new_users_with_roles": [
+ *          "exface.Core.SUPERUSER"
+ *      ]
+ *  }
+ * 
+ * ```
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class MicrosoftOAuth2Autenticator extends OAuth2Authenticator
 {
     use MicrosoftOAuth2Trait {
