78a90c9c0cf74a74d89ef63b4ac2fa21cb281869
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-06-14 14:30:08 +0200

    NEW meta object for azure groups

--- CommonLogic/Security/Authenticators/MicrosoftOAuth2Autenticator.php
+++ CommonLogic/Security/Authenticators/MicrosoftOAuth2Autenticator.php
@@ -38,6 +38,8 @@ use exface\Core\Exceptions\UnexpectedValueException;
  * 
  * ## Example Configuration
  * 
+ * ### SSO with static roles
+ * 
  * ```
  *  {
  *      "class": "\\axenox\\Microsoft365Connector\\CommonLogic\\Security\\Authenticators\\MicrosoftOAuth2Autenticator",
@@ -48,6 +50,66 @@ use exface\Core\Exceptions\UnexpectedValueException;
  *      "create_new_users": true,
  *      "create_new_users_with_roles": [
  *          "exface.Core.SUPERUSER"
+ *      ]
+ *  }
+ * 
+ * ```
+ * 
+ * ### SSO with role sync via OAuth2 token
+ * 
+ * Azure AD can be configured to send the list of Azure groups assigned to the user inside the OAuth token.
+ * If you set up external roles in the workbench with the UIDs of ceratin Azure groups, you can "remote control"
+ * user roles by assigning the corresponding groups in Azure.
+ * 
+ * **NOTE:** this only works for "internal" Azure groups assigned within azure. This does not work for groups
+ * uploaded to azure from external sources like an on-premise AD instance, an IAM tool, or similar. These
+ * external groups will simply be missing in the list. To read them too, use Microsoft Graph as described below.
+ * 
+ * Here is how to make Azure send groups inside the token
+ * 
+ * - Navigate to "Token configuration" and click on "add groups claim".
+ * - Select "Security groups" and "Group ID" on "Customize token properties by type" (Screenshot 6)
+ * - Optionally make sure the complete name of the user is included in the token: Create another 
+ * claim via "Add optional claim" and choose "Token type: ID". Select the claim "family_name" to 
+ * make sure that the last name of the user is sent via IDToken to the application.
+ * 
+ * This will allow Azure to send the group ID via an IDToken when logging in to the application (Screenshot 7). 
+ * This ID can be matched with the roles created in the application. The users will then be logged in with the 
+ * same roles and permittions that they have be assigned to in Azure.
+ * 
+ * Notice that the group IDs will only be sent via the IDToken for users created directly within Azure. The 
+ * token will not include group IDs if an external user is trying to sign in to the application.
+ * 
+ * ```
+ *  {
+ *      "class": "\\axenox\\Microsoft365Connector\\CommonLogic\\Security\\Authenticators\\MicrosoftOAuth2Autenticator",
+ *      "id": "AZURE_AD",
+ *      "name": "Azure AD",
+ *      "client_id": "552b11b2-586d-4154-a67b-c57af5a7ccce",
+ *      "client_secret": "fx2NG7jjy0JK8_8l.G-0lXup_T9F7W_iWm",
+ *      "create_new_users": true,
+ *      "sync_roles": true
+ *  }
+ * 
+ * ```
+ * 
+ * ### SSO with role sync via Microsoft Graph
+ * 
+ * **IMPORTANT:** the configuration of the data connection for Microsoft Graph (client_id, secret, tenant, etc.)
+ * MUST be identical with that of the authenticator!
+ * 
+ * ```
+ *  {
+ *      "class": "\\axenox\\Microsoft365Connector\\CommonLogic\\Security\\Authenticators\\MicrosoftOAuth2Autenticator",
+ *      "id": "AZURE_AD",
+ *      "name": "Azure AD",
+ *      "client_id": "552b11b2-586d-4154-a67b-c57af5a7ccce",
+ *      "client_secret": "fx2NG7jjy0JK8_8l.G-0lXup_T9F7W_iWm",
+ *      "create_new_users": true,
+ *      "sync_roles": true,
+ *      "sync_roles_via_ms_graph": true,
+ *      "share_token_with_connections": [
+ *          "my.App.ConnectionToMicrosoftGraph"
  *      ]
  *  }
  * 
