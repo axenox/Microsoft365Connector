47b4b2aaa8cdea802c522b56124ea43b0eb24986
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-08-13 12:41:18 +0200

    DOCS added for MicrosoftOAuth2 data source authentication

--- DataConnectors/Authentication/MicrosoftOAuth2.php
+++ DataConnectors/Authentication/MicrosoftOAuth2.php
@@ -2,11 +2,30 @@
 namespace axenox\Microsoft365Connector\DataConnectors\Authentication;
 
 use exface\Core\CommonLogic\UxonObject;
-use axenox\OAuth2Connector\CommonLogic\Security\AuthenticationToken\OAuth2AuthenticatedToken;
 use axenox\OAuth2Connector\DataConnectors\Authentication\OAuth2;
 use axenox\Microsoft365Connector\CommonLogic\Security\Authenticators\MicrosoftOAuth2Trait;
 use TheNetworg\OAuth2\Client\Token\AccessToken;
 
+/**
+ * Authenticates in a data source using the Microsoft OAuth 2.0 protocol (e.g. in Microsoft Graph)
+ * 
+ * ## Examples
+ * 
+ * Here is an example configuration for the Microsoft Graph API using an Azure App Registration for authentication.
+ * 
+ * ```json
+ * {
+ *  "url": "https://graph.microsoft.com/v1.0/",
+ *  "authentication": {
+ *      "class": "\\axenox\\Microsoft365Connector\\DataConnectors\\Authentication\\AzureAppRegistrationAuth",
+ *      "client_id": "...",
+ *      "client_secret": "...",
+ *      "tenant": "...",
+ *      "scope": "https://graph.microsoft.com/.default"
+ *  }
+ * }
+ * ```
+ */
 class MicrosoftOAuth2 extends OAuth2
 {
     use MicrosoftOAuth2Trait {
